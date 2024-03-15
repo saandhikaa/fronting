81b952d9b677cb4c7ea3268166dee062ebe21705
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-03-15 16:38:00 +0700

    create login page

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Login: FC = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-blue-500 mb-2'>Login</h1>
+    </div>
+  );
+}
+
+export default Login;
