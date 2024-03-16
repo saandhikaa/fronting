c7b4b7496c45860beaf4c54a7b1a0b38c6c77391
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-03-16 17:13:31 +0700

    use components\InputField

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,44 +1,30 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
+import InputField from '../components/InputField';
 
 const Login: FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Handle login logic here
-  };
-
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200">
-      <div className="p-6 bg-white rounded shadow-md w-80">
-        <form onSubmit={handleLogin}>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            autoComplete="off"
-            onChange={(e) => setEmail(e.target.value)}
-            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-          />
-          <label htmlFor="password" className="block mt-4 text-sm font-medium text-gray-700">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-          />
-          <button type="submit" className="w-full px-4 py-2 mt-4 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-            Login
-          </button>
-        </form>
-      </div>
+    <div className="flex flex-col items-center h-screen pt-16 bg-gray-200">
+      <div className='w-6/12 h-32 mb-4 bg-white'></div>
+      <form className='flex flex-col items-center'>
+        <InputField
+          type='email'
+          name='email'
+          placeholder='Email'
+          autoComplete='off'
+          className='px-4 py-1 mb-4 w-72'
+        />
+
+        <InputField
+          type='password'
+          name='password'
+          placeholder='Password'
+          className='px-4 py-1 mb-4 w-72'
+        />
+        
+        <button type="submit" className="px-10 py-1 font-medium text-red-600 bg-white border border-transparent shadow-sm py-1text-sm hover:bg-red-700 hover:text-white">
+          Login
+        </button>
+      </form>
     </div>
   );
 };
