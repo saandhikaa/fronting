4b8bd4d2e1e936debafdb9c9fabb7c487b410ffc
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-03-21 11:16:46 +0700

    update component dropdown: use navigation

--- client/src/components/DropDown.tsx
+++ client/src/components/DropDown.tsx
@@ -1,31 +1,37 @@
 import { FC, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface DropdownProps {
   text: string;
-  options: string[];
-  onSelect: (option: string) => void;
+  options: { label: string; path: string }[];
 }
 
-const Dropdown: FC<DropdownProps> = ({ text, options, onSelect }) => {
+const Dropdown: FC<DropdownProps> = ({ text, options }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSelect = (path: string) => {
+    setIsOpen(false);
+    navigate(path);
+  };
 
   return (
     <div className="relative">
-      <button onClick={() => setIsOpen(!isOpen)} className="p-2">
+      <button onClick={() => setIsOpen(!isOpen)} className="py-1 ">
         {text}
       </button>
       {isOpen && (
-        <ul>
-          {options.map((option, index) => (
-            <li
-              key={index}
-              onClick={() => onSelect(option)}
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+        <div className="mb-2">
+          {options.map((option) => (
+            <button
+              key={option.label}
+              onClick={() => handleSelect(option.path)}
+              className="block px-4 py-1 text-sm text-gray-700 hover:bg-gray-100"
             >
-              {option}
-            </li>
+              {option.label}
+            </button>
           ))}
-        </ul>
+        </div>
       )}
     </div>
   );
