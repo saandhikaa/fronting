14e89e9f7888a34c6ac17bda23955fd8b67a9057
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-03-21 12:05:12 +0700

    update component inputField: add label

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -1,12 +1,15 @@
 import { FC, InputHTMLAttributes } from 'react';
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  size: number;
   name: string;
-  className?: string;
 }
 
-const InputField: FC<InputFieldProps> = ({ name, className, ...props }) => (
-  <input id={name} name={name} {...props} className={className}/>
+const InputField: FC<InputFieldProps> = ({ size, name, ...props }) => (
+  <div className='flex mb-4 -translate-x-14'>
+    <label htmlFor={name} className='w-28'>{name.charAt(0).toUpperCase() + name.slice(1)}</label>
+    <input id={name} name={name} {...props} className={`w-${size} h-8 px-2`}/>
+  </div>
 );
 
 export default InputField;
