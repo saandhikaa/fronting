df120b9ebff754fb9058cb580fb4c97c493bf89f
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-03-21 12:06:11 +0700

    update page login: follow the component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,29 +1,33 @@
 import { FC } from 'react';
 import InputField from '../components/InputField';
+import Button from '../components/Button';
 
 const Login: FC = () => {
   return (
     <div className="flex flex-col items-center h-screen pt-16 bg-gray-200">
-      <div className='w-6/12 h-32 mb-4 bg-white'></div>
-      <form className='flex flex-col items-center'>
+      <div className='w-2/3 h-32 mb-4 bg-white'></div>
+      <form action='/dashboard' className='flex flex-col items-center'>
         <InputField
-          type='email'
-          name='email'
-          placeholder='Email'
+          size={72}
+          type='text'
+          name='username'
+          placeholder='Your username'
           autoComplete='off'
-          className='px-4 py-1 mb-4 w-72'
         />
 
         <InputField
+          size={72}
           type='password'
           name='password'
-          placeholder='Password'
-          className='px-4 py-1 mb-4 w-72'
+          placeholder='Your password'
         />
         
-        <button type="submit" className="px-10 py-1 font-medium text-red-600 bg-white border border-transparent shadow-sm py-1text-sm hover:bg-red-700 hover:text-white">
-          Login
-        </button>
+        <Button
+          size={72}
+          text="Login"
+          type="submit"
+          color="green"
+        />
       </form>
     </div>
   );
