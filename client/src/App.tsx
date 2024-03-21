9eb0dcde1304117655ca0c276a33db6dbaa5b554
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2024-03-21 11:18:58 +0700

    update App: import page dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
 import { FC } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { routes } from './routes';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 const App: FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.login} element={<Login />} />
+        <Route path={routes.dashboard} element={<Dashboard />} />
       </Routes>
     </Router>
-    
   );
 }
 
